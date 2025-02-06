3e8619b2ec0d9948ecd29d7bac6eea43e3f55604
Hari Sekhon <harisekhon@gmail.com>
2025-02-07 00:01:27 +0700

    updated ci_bootstrap.sh

--- setup/ci_bootstrap.sh
+++ setup/ci_bootstrap.sh
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2020-06-02 17:43:35 +0100 (Tue, 02 Jun 2020)
 #
-#  https://github.com/HariSekhon/Vagrant-templates
+#  https://github.com/HariSekhon/Vagrant
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
